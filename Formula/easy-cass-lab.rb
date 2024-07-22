d1ab8ad04553322f6506dc6f0e9b166776a07c60
Jon Haddad <jon@jonhaddad.com>
2024-07-22 16:58:28 -0700

    Extracted the version to a separate variable to make updates easier

--- Formula/easy-cass-lab.rb
+++ Formula/easy-cass-lab.rb
@@ -1,7 +1,8 @@
 class EasyCassLab < Formula
+  version="2"
   desc "easy-cass-lab: a tool for creating Cassandra clusters"
   homepage "https://github.com/rustyrazorblade/easy-cass-lab"
-  url "https://github.com/rustyrazorblade/easy-cass-lab/releases/download/v2/easy-cass-lab-2.tar.gz"
+  url "https://github.com/rustyrazorblade/easy-cass-lab/releases/download/v#{version}/easy-cass-lab-#{version}.tar.gz"
   sha256 "82a84bacf437bb39a79add1103d2d33914f1a9e9c4d217bc6e1c1aa5e556eed9"
   license "Apache"
 
