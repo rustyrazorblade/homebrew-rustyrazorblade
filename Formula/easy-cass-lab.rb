e1d356f8d8d1073b44a8c725cbb77351f212966b
Jon Haddad <jon@jonhaddad.com>
2025-01-13 11:29:47 -0800

    Version bump to 10

--- Formula/easy-cass-lab.rb
+++ Formula/easy-cass-lab.rb
@@ -1,9 +1,9 @@
 class EasyCassLab < Formula
-  version "9"
+  version "10"
   desc "easy-cass-lab: a tool for creating Cassandra clusters"
   homepage "https://github.com/rustyrazorblade/easy-cass-lab"
   url "https://github.com/rustyrazorblade/easy-cass-lab/releases/download/v#{version}/easy-cass-lab-#{version}.tar.gz"
-  sha256 "7a5d2bf1db210ed2062270570410a9872313d2d233a5516d1bf90a1ff14d5947"
+  sha256 "e3a3b34544f8b966fa1374a4f616270ccce7cec0dbf66463e6bb4e32a62c0f76"
   license "Apache"
 
   def install
