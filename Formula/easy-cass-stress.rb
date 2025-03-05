3519a6bf14d907255d3445a57271652dcccb2e8d
Jon Haddad <jon@jonhaddad.com>
2025-03-04 17:34:25 -0800

    Version bump to 8

--- Formula/easy-cass-stress.rb
+++ Formula/easy-cass-stress.rb
@@ -2,7 +2,7 @@ class EasyCassStress < Formula
   desc "easy-cass-stress: a tool for benchmarking Cassandra clusters"
   homepage "https://github.com/rustyrazorblade/easy-cass-stress"
   url "https://github.com/rustyrazorblade/easy-cass-stress/releases/download/v6.2.0-SNAPSHOT/easy-cass-stress-6.2.0-SNAPSHOT.tar.gz"
-  sha256 "baff138777743c1908944dc4477b3959aa53d841f0327a9346aff2276186dd6e"
+  sha256 "7d3d47b4779eea1cb91d8affa82cffce6fc8540efed2c8e54d6f326b59713377"
   license "Apache"
 
   def install
